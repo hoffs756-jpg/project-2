67add142b69abf781564df21c5c162eb0d13ef1c
hoffs756-jpg <hoffs756@newschool.edu>
2026-03-30 02:36:47 +0000

    sdf

--- entry-1/script.js
+++ entry-1/script.js
@@ -10,10 +10,7 @@ function hex2rgb(hex) {
 
 function rgb2hex(rgb) {
   rgb = rgb.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
-
-  function hex(x) {
-    return ("0" + parseInt(x).toString(16)).slice(-2);
-  }
+  function hex(x) { return ("0" + parseInt(x).toString(16)).slice(-2); }
   return "#" + hex(rgb[1]) + hex(rgb[2]) + hex(rgb[3]);
 }
 
@@ -21,41 +18,25 @@ function rgbArray(rgb) {
   return rgb.split('(')[1].split(')')[0].split(',');
 }
 
-//
-// finds transition colour based on mouse position
 function transitionColour(from, to, width, x) {
-
   var m = x / width;
-  var r, g, b;
-
-  r = Math.ceil(from[0] * m + to[0] * (1 - m));
-  g = Math.ceil(from[1] * m + to[1] * (1 - m));
-  b = Math.ceil(from[2] * m + to[2] * (1 - m));
-
+  var r = Math.ceil(from[0] * m + to[0] * (1 - m));
+  var g = Math.ceil(from[1] * m + to[1] * (1 - m));
+  var b = Math.ceil(from[2] * m + to[2] * (1 - m));
   return rgb2hex('rgb(' + r + ', ' + g + ', ' + b + ')');
-
 }
 
-$(document).on("mousemove", function(e) {
+document.addEventListener("mousemove", function(e) {
+  var xPos = e.pageX;
+  var width = window.innerWidth;
 
-  var xPos = e.pageX,
-    width = window.innerWidth,
+  var topLeft     = hex2rgb(colours[0][0]);
+  var topRight    = hex2rgb(colours[1][0]);
+  var bottomLeft  = hex2rgb(colours[0][1]);
+  var bottomRight = hex2rgb(colours[1][1]);
 
-    //
-    // convert hex to rgb
-    topLeft = hex2rgb(colours[0][0]),
-    topRight = hex2rgb(colours[1][0]),
-
-    bottomLeft = hex2rgb(colours[0][1]),
-    bottomRight = hex2rgb(colours[1][1]);
-
-  //
-  // get transition colour
+  var topTransition    = transitionColour(rgbArray(topRight), rgbArray(topLeft), width, xPos);
   var bottomTransition = transitionColour(rgbArray(bottomRight), rgbArray(bottomLeft), width, xPos);
-  var topTransition = transitionColour(rgbArray(topRight), rgbArray(topLeft), width, xPos);
 
-  $('body').css({
-    'background': 'linear-gradient(' + topTransition + ', ' + bottomTransition + ')'
-  });
-
+  document.body.style.background = 'linear-gradient(' + topTransition + ', ' + bottomTransition + ')';
 });
